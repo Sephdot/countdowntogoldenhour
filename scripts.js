d549b29ad0004623b9a833977d0ef4569185fbe3
Joseph Eaton <josepheaton04@outlook.com>
2025-02-17 12:12:25 +0000

    typo

--- scripts.js
+++ scripts.js
@@ -5,5 +5,5 @@ navigator.geolocation.getCurrentPosition(position => console.log(`${position.lat
 document.body.addEventListener('load', handlePageLoad);
 
 function handlePageLoad() {
-	navigator.geolocation.getCurrentPosition(position => console.log(`${position.latitude}, ${position.longitude}`);
+	navigator.geolocation.getCurrentPosition(position => console.log(`${position.latitude}, ${position.longitude}`));
 }
