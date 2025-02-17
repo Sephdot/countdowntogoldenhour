9538cccb63520a74bc0dc91934f3555bb0e5f3c1
Joseph Eaton <josepheaton04@outlook.com>
2025-02-17 12:34:45 +0000

    add error handling to requestGeolocation

--- scripts.js
+++ scripts.js
@@ -1,5 +1,21 @@
-window.addEventListener('load', handlePageLoad);
+window.addEventListener('load', requestGeolocation);
 
-function handlePageLoad() {
-	navigator.geolocation.getCurrentPosition(position => console.log(`${position.coords.latitude}, ${position.coords.longitude}`));
+function requestGeolocation() {
+	navigator.geolocation.getCurrentPosition(
+	(position) => {
+		console.log(`${position.coords.latitude}, ${position.coords.longitude}`);
+	},
+	(error) => {
+		switch(error.code) {
+			case error.PERMISSION_DENIED:
+				alert("Access denied! Change settings please :)"); break;
+			case error.POSITION_UNAVAILABLE:
+				alert("Position can't be found - check your connection or enable GPS!"); break;
+			case error.TIMEOUT:
+				alert("Timed out - try again!"); break;
+			default:
+				alert("Oops! Something strange happened. Try again!");
+		}
+	}
+	);
 }
